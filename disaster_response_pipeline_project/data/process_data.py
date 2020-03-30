2f5df62bd468c86162cef269e973f4d5db919b76
RAMCHAND_N <nitin.ramchand-lalwani@airbus.com>
2020-03-30 12:32:06 +0200

    Update process_data.py
    The data has been loaded and the dataframe has been cleaned so far.

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,17 +1,64 @@
 import sys
-import pandas
-
-def probando(hola):
-    if hola == 'hola':
-        print ('buenaz tardez')
+import pandas as pd
+import numpy as np
+from unittest.mock import inplace
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    # This functions loads the messages and the categories datasets which are in csv format 
+    # into pandas dataframes and the finally returns a merged dataframe on the 'id' column
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(messages_filepath)
+    df = messages.merge(categories, how='outer', on='id')
+    
+    return df
 
 def clean_data(df):
-    pass
-
+    # This function takes the merged dataframe as input and cleans it up before saving it
+    # into a database.
+    
+    # categories will be a dataframe with all the values expanded of the categories column
+    categories = df['categories'].str.split(';',expand=True)
+    
+    # From the expanded Dataframe defined above we will extract the list with all the 
+    # column names
+    new_catergory_columns = []
+    
+    # This is just the first row of the dataframe
+    row = categories.iloc[0]
+    
+    # Then to extract the column names we take the first part of the string when we split it at the -
+    
+    for cat in row.str.split('-'):
+        new_catergory_columns.append(cat[0])
+        
+    # Now we update the column names of the categories dataframe
+    categories.columns = new_catergory_columns
+    
+    for cat in new_catergory_columns:
+        # For each category we change te value in the dataframe into the last character of the string
+        # which indicated whether its a 1 or a 0
+        categories[cat] = categories[cat].str[-1]
+        
+        # We also convert this string into an integer
+        categories[cat] = pd.to_numeric(categories[cat])
+        
+    # Now we add the 'id' row in the categories Dataframe in order to then merge it with df
+    categories['id'] = df['id']
+    
+    # The following step is to drop the categories column in the df
+    df.drop(columns='categories', inplace=True)
+    
+    # Now we merge df with the categories dataframe
+    df = df.merge(categories, how='outer', on='id')
+    
+    #We deal with duplicated rows
+    print ('nº of duplciated items before deleting duplicates are', df.duplicated().sum())
+    print('shape of dataframe is', df.shape)
+    df.drop_duplicates(inplace=True)
+    print ('nº of duplciated items after deleting duplicates are', df.duplicated().sum())
+    print('shape of dataframe is', df.shape)
+    
+    return df
 
 def save_data(df, database_filename):
     pass  
